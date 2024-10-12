b172b1fb1d9a823a81424eb995c491acb2aeca5f
ysrajsingh <ysgahir1925@hotmail.com>
2024-10-12 22:41:21 +0530

    hot boot setup

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from fastapi.openapi.utils import get_openapi
+from schemas.index import Quote
+
 
 app = FastAPI()
+load_dotenv()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+api_routers = []
+
+
+for router in api_routers:
+    app.include_router(router, prefix="/api")
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    quote_of_the_time = Quote()
+    return {
+        "success": True,
+        "message": quote_of_the_time.message,
+    }
