172963ac39880d9ea869092aa535cb9443363743
ysrajsingh <ysgahir1925@hotmail.com>
2024-10-15 20:22:02 +0530

    in progress

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+import os
+from middleware import JWTMiddleware
+from endpoints import users
 from fastapi.openapi.utils import get_openapi
 from schemas.index import Quote
+
 
 
 app = FastAPI()
@@ -16,10 +20,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.add_middleware(
+    JWTMiddleware,
+    secret_key=os.getenv('SECRET_KEY'),
+    algorithm=os.getenv('ALGORITHM')
+)
 
-
-api_routers = []
-
+api_routers = [
+    users.router,
+]
 
 for router in api_routers:
     app.include_router(router, prefix="/api")
@@ -32,3 +41,34 @@ async def root():
         "success": True,
         "message": quote_of_the_time.message,
     }
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    
+    openapi_schema = get_openapi(
+        title="Gen Y Projects",
+        version="0.0.1",
+        routes=app.routes
+    )
+
+    # Define the security scheme
+    openapi_schema["components"]["securitySchemes"] = {
+        "BearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+        }
+    }
+
+    # Add security requirements to each route
+    for route in openapi_schema["paths"].values():
+            for method in route.values():
+                method["security"] = [{"BearerAuth": []}]
+
+    
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
